462df4aa93575d54a2eaca4fe565dc2b20053343
Ahmed Shahram <shahram.ask.546@gmail.com>
2023-09-18 15:37:45 +0500

    create seeder for permissions

--- spatie-app/database/seeders/DatabaseSeeder.php
+++ spatie-app/database/seeders/DatabaseSeeder.php
@@ -7,12 +7,14 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        \App\Models\User::factory(10)->create();
-        // $this->call(UserSeeder::class);
-    }
+	/**
+	 * Seed the application's database.
+	 */
+	public function run(): void
+	{
+		// \App\Models\User::factory(10)->create();
+		// run permission seeder
+		$this->call(PermissionSeeder::class);
+		$this->call(UserSeeder::class);
+	}
 }

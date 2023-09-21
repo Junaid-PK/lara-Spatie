f13215f452947fd88dae0a2c9da6533b23a5552f
Ahmed Shahram <shahram.ask.546@gmail.com>
2023-09-21 12:32:48 +0500

    create get last team messages controller

--- spatie-app/app/Http/Controllers/MessageController.php
+++ spatie-app/app/Http/Controllers/MessageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Events\MessageEvent;
+use App\Http\Requests\Message\GetMessageRequest;
 use App\Http\Requests\Message\PostMessageRequest;
 use App\Models\Message;
 use App\Models\Team;
@@ -10,7 +11,9 @@ use App\Models\TeamMember;
 use App\Models\User;
 use App\Services\MessageService;
 use App\Services\TeamMemberService;
+use App\Services\TeamService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MessageController extends Controller
 {
@@ -30,8 +33,25 @@ class MessageController extends Controller
 		return ['status' => 'Message Sent Successfully'];
 	}
 
-	public function getMessages()
+	public function getLastMessages(GetMessageRequest $request, TeamService $teamService)
 	{
-		// 
+		$validated = $request->validated();
+		$team = $teamService->showTeam($validated['team_id']);
+		if (!$team) {
+			return response()->json([
+				'message' => 'Team not found'
+			], 404);
+		}
+
+		$rawQuery = 'SELECT m.id, m.message, m.teammember_id, m.created_at FROM messages m WHERE m.teammember_id IN (SELECT tm.id FROM teammembers tm WHERE tm.team_id = ' . $validated['team_id'] . ')';
+		if (isset($validated['last_message_id'])) {
+			$rawQuery .= ' AND m.id < ' . $validated['last_message_id'];
+		}
+		$rawQuery .= ' ORDER BY m.id DESC LIMIT 15';
+		$messages = DB::select($rawQuery);
+
+		return response()->json([
+			'messages' => $messages
+		], 200);
 	}
 }
